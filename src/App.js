a395ae03d9a828c07043fe87565fd555264ceb22
Paige Sylvan <paigesylvan@gmail.com>
2023-08-18 13:05:06 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import schema from './Validation/formSchema';
+import * as yup from 'yup';
+
+import Form from './Components/Form'
+
+const initialFormValues = {
+  username: '',
+  password: '',
+  email: '',
+  tos: false,
+}
+
+const initialFormErrors = {
+  username: '',
+  password: '',
+  email: '',
+  tos: '',
+}
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [users, setUsers] = useState([])
+
+  const handleSubmit = () => {
+    axios.post('https://reqres.in/api/users', formValues)
+      .then(res => {
+        setUsers([ res.data, ...users])
+      })
+      .catch(err => console.error.err)
+      .finally(() => setFormValues(initialFormValues))
+  }
+
+  const validate = (name, value) =>{
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+
+
+  const handleChange = (name, value) => {
+    validate(name, value)
+    setFormValues({...formValues, [name]: value})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form values={formValues}
+      change={handleChange}
+      errors={formErrors}
+      submit={handleSubmit}
+      />
+
+      {users.map(user =>(
+        <div key={user.id}> 
+          <p>{user.createdAt}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
